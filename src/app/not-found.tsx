d0398773e0afde9bc53d669f3704cc218c258b7f
Mike Odnis <airwolf635@gmail.com>
2023-07-31 05:49:12 -0400

    Quick commit

---
+++ src/app/not-found.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+import { Button } from '@/components/ui/button'
+type Props = {}
+
+const NotFound = (props: Props) => {
+  return (
+    <>
+      <div className="flex items-center justify-center h-screen bg-[rgb(36,36,36)] text-white flex-col">
+        <div className='flex items-center justify-center'>
+          <div className="loader"/>
+          <Image
+            className="absolute select-none h-60 w-60 animate-pulse"
+            src="/MyLogo.svg"
+            alt="Loading..."
+            width={100}
+            height={100}
+          />
+        </div>
+        <Link href={`/`}>
+          <Button className={`mt-20 w-[200px] bg-transparent px-6 py-2 border border-purple-600 rounded-sm uppercase text-sm tracking-widest text-purple-400 transition-all hover:border-purple-600/40 hover:text-purple-400/40 hover:bg-transparent`}>
+            Go Home
+          </Button>
+        </Link>
+      </div>
+
+    </>
+  )
+}
+export default NotFound
--- src/components/Skill.tsx
+++ src/components/Skill.tsx
@@ -6,12 +6,12 @@ import Link from 'next/link'
 export default function Skill() {
   return (
     <>
-      {Skills.map((skill, index) => (
+      {Skills.map((skill) => (
         <div key={skill.name} className='relative flex cursor-pointer group'>
           {skill.certificate ? (
             <Link href={skill.certificate} target='_blank' rel={`noopener noreferrer`}>
             <Image
-              className={`object-contain h-24 transition duration-300 ease-in-out border ${skill.certificate? `border-[#BA9BDD]` : `border-gray-500`}  rounded-md w-[150px] xl:w-[100px] xl:h-[100px] filter grayscale-0 group-hover:grayscale md:w-28 md:h-28`}
+              className={`object-contain h-24 transition duration-300 ease-in-out border border-gray-500 ${skill.certificate? `hover:bg-[#BA9BDD]` : ``}  rounded-md w-[150px] xl:w-[100px] xl:h-[100px] filter grayscale-0 group-hover:grayscale md:w-28 md:h-28`}
               src={skill.image} alt={`${skill.name} skill badge`} width={200} height={200}/>
               </Link>
             ): (
