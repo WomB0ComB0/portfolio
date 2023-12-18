ef2524cd5965024f0a0e1aeae0ef170bd51caefe
Mike Odnis <95197809+WomB0ComB0@users.noreply.github.com>
2023-12-17 19:52:48 -0500

    🧹: Liveblocks configuration
    Going to revamp my portfolio a bit. So it meets next14 standards, and best practices.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,9 +4,16 @@ import './global.scss'
 import React, { useState, useEffect } from 'react';
 import { Loader } from '@/components';
 import Head from 'next/head';
+// import { RoomProvider, useMyPresence } from '../../liveblocks.config';
+// import LiveCursors from '@/components/cursor/LiveCursors';
+// import { useRouter } from 'next/router';
+// import { useMemo, useRef } from 'react';
 import { Analytics } from '@vercel/analytics/react';
+
+
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   const [isLoading, setIsLoading] = useState(true);
+  // const roomId = useOverrideRoomId("nextjs-live-cursors-advanced");
   useEffect(() => {
     const timeout = setTimeout(() => {
       setIsLoading(false);
@@ -32,11 +39,44 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
         <Loader />
       ) : (
         <>
+          {/* <RoomProvider
+              id={roomId}
+              Initialize the cursor position to null when joining the room
+              initialPresence={{
+                cursor: null,
+              }}
+           > */}
           {children}
           <Analytics />
+          {/* </RoomProvider> */}
         </>
       )}
     </>
   );
 };
 export default RootLayout;
+
+// function Example() {
+//   const cursorPanel = useRef(null);
+//   const [{ cursor }] = useMyPresence();
+
+//   return (
+//     <main ref={cursorPanel} className={styles.main}>
+//       <LiveCursors cursorPanel={cursorPanel} />
+//       <div className={styles.text}>
+//         {cursor
+//           ? `${cursor.x} × ${cursor.y}`
+//           : "Move your cursor to broadcast its position to other people in the room."}
+//       </div>
+//     </main>
+//   );
+// }
+
+// function useOverrideRoomId(roomId: string) {
+//   const { query } = useRouter();
+//   const overrideRoomId = useMemo(() => {
+//     return query?.roomId ? `${roomId}-${query.roomId}` : roomId;
+//   }, [query, roomId]);
+
+//   return overrideRoomId;
+// }
