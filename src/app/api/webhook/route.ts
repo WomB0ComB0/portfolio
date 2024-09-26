b2b472644477cc0edb1472c7b66f6af90e47ffbd
Mike Odnis <airwolf635@gmail.com>
2024-09-26 17:51:28 -0400

    chore: quick commit

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -8,7 +8,7 @@ import {
   manageSubscriptionStatusChange
 } from './admin';
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY!, {
   apiVersion: '2024-06-20',
 });
 
--- src/utils/stripe.ts
+++ src/utils/stripe.ts
@@ -1,9 +1,9 @@
 import Stripe from 'stripe';
 
-if (!process.env.STRIPE_SECRET_KEY) {
+if (!process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY) {
   throw new Error('STRIPE_SECRET_KEY is not set in the environment variables');
 }
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+export const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY, {
   apiVersion: '2024-06-20', // Use the latest API version or the one you prefer
 });
