0ca32a23a0fbf4cb5e0dc1cd9d6d78e89993c653
Mike Odnis <airwolf635@gmail.com>
2024-10-02 17:37:11 -0400

    chore: update workflows

--- src/e2e/example.spec.ts
+++ src/e2e/example.spec.ts
@@ -1,7 +1,11 @@
-import { expect, test } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('./');
+test('visit page and take screenshot', async ({ page }) => {
+  const targetUrl = process.env.ENVIRONMENT_URL || 'https://www.mikeodnis.dev'
 
-  await expect(page).toHaveTitle(/Mike Odnis/);
-});
+  const response = await page.goto(targetUrl)
+
+  expect(response.status(),'should respond with correct status code').toBeLessThan(400)
+
+  await page.screenshot({ path: 'screenshot.jpg' })
+})
--- vitest.config.ts
+++ vitest.config.ts
@@ -6,5 +6,8 @@ export default defineConfig({
   plugins: [react()],
   test: {
     environment: 'jsdom',
+    exclude: ['src/e2e/**', 'node_modules'],
+  
   },
 });
+
