dbc97101c041ca6f1b2450d0df9f70c87dbb0c63
Mike Odnis <mike@mikeodnis.dev>
2025-10-24 20:28:46 -0400

    refactor(global): ♻️ Declare `isNotNull` and `sleep` as functions
    The change declares `isNotNull` and `sleep` as functions instead of constants.

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -415,7 +415,7 @@ type AllArguments<T> = T extends (...args: infer Args) => any ? Args : never;
  * isNotNull(5); // true
  * isNotNull(null); // false
  */
-const isNotNull = <Value>(value: Value): value is Exclude<Value, null> => {
+declare function isNotNull <Value>(value: Value): value is Exclude<Value, null> {
   return value !== null;
 };
 
@@ -429,7 +429,7 @@ const isNotNull = <Value>(value: Value): value is Exclude<Value, null> => {
  * @example
  * await sleep(1000); // Waits for 1 second
  */
-const sleep = <A extends number, R extends void>(n: A): Promise<R> => {
+declare function sleep <A extends number, R extends void>(n: A): Promise<R> {
   return new Promise((resolve) => setTimeout(resolve, n));
 };
 
