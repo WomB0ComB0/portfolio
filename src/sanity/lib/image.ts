ccfeb0653e6eb238d02880dca679f84064bb0b54
Mike Odnis <mike@mikeodnis.dev>
2025-12-02 13:19:17 +0000

    refactor: update import statement for createImageUrlBuilder to use named import

--- src/sanity/lib/image.ts
+++ src/sanity/lib/image.ts
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-import createImageUrlBuilder from '@sanity/image-url';
+import { createImageUrlBuilder } from '@sanity/image-url';
 import type { SanityImageSource } from '@sanity/image-url';
 
 import { dataset, projectId } from '../env';
